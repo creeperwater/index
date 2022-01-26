95e669951921f786d9268aa431a908d126bc5314
CreeperWater <3281213780@qq.com>
2022-01-26 08:35:50 +0800

    1

--- cpi/index.js
+++ cpi/index.js
@@ -1,10 +1,15 @@
-$('.world .banner div').click(function(){
+$('.world .banner>div').click(function(){
     var target = $(this).attr('goto');
     $('.world .content>div').each(function(){$(this).hide();});
     $(target).show();
 });
-$.getJSON("data/world.json", function (data) {
+$.getJSON("data/world2020.json", function (data) {
     for(var i=0,len=data.length;i<len;i++){
-        $('.world .chart1').append(`<div><h1>${data[i].rank}</h1><h2>${data[i].country}</h2><p><span style='width:${data[i].score}%'></span></p></div>`);
+        $('.world .chart1').append(`<div><h1>${data[i].rank}</h1><h2>${data[i].ISO3}</h2><p><span style='width:${data[i].score}%'></span></p></div>`);
+    }
+})
+$.getJSON("data/world2019.json", function (data) {
+    for(var i=0,len=data.length;i<len;i++){
+        $('.world .chart2').append(`<div><h1>${data[i].rank}</h1><h2>${data[i].ISO3}</h2><p><span style='width:${data[i].score}%'></span></p></div>`);
     }
 })
